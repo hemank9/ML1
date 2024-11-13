f0e1a8363ffe5db6384a4baa4dbcc1150d73cee7
hemank9 <hemank9697@gmail.com>
2024-11-13 20:20:11 +0530

    first setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this func will return the list of requirements
+    '''
+
+    requirements =[]
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n',  "") for req in requirements]
+
+    return requirements
+
+
+
+setup(
+
+name='ML1',
+version='0.0.1',
+author='hemank9',
+author_email='hemank9697@gmail.com',
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+
+)
